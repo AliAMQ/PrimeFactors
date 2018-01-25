1895bc1aec5487b87ee787b579a01d9212f8fe37
AliAMQ <alireza.amq@gmail.com>
2018-01-25 15:42:25 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(primeFactors(14));
+    }
+
+    public static ArrayList<Integer> primeFactors(int a) {
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        for(int i=2;i<a-2;i++){
+            if(isPrime(i) && isPrime(a-i)){
+                primes.add(i);
+                primes.add(a-i);
+                return primes;
+            }
+        }
+        return primes;
+    }
+
+    public static boolean isPrime(int number){
+        for (int i =2; i*i <= number;i++){
+            if (number%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
